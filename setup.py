eed4ea2d787cf4cf07ec9d5a95b27f2c352f271a
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-02-10 14:35:35 -0500

    Add Git link instead of name

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name='blocks.contrib',
     namespace_packages=['blocks'],
-    install_requires=['blocks'],
+    install_requires=['git+git://github.com/bartvm/blocks.git#egg=blocks'],
     packages=find_packages(exclude=['examples']),
     zip_safe=False)
